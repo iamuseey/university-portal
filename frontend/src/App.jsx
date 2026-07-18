3f74b82fb353acc3c529c68719455d27dcfd0475
Usman Ibrahim <Ussey353@gmail.com>
2026-07-18 12:11:28 +0100

    Add nodemon and protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,22 +3,22 @@ import StudentLogin from './pages/StudentLogin'
 import StaffLogin from './pages/StaffLogin'
 import AdminLogin from './pages/AdminLogin'
 import StudentDashboard from './pages/StudentDashboard'
+import ProtectedRoute from './ProtectedRoute'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Student Login - Public */}
         <Route path="/login" element={<StudentLogin />} />
-
-        {/* Staff Login - Internal */}
         <Route path="/staff/login" element={<StaffLogin />} />
-
-        {/* Admin Login - Private */}
         <Route path="/admin/login" element={<AdminLogin />} />
 
-        {/* Student Dashboard */}
-        <Route path="/student/dashboard" element={<StudentDashboard />} />
+        {/* Protected Route */}
+        <Route path="/student/dashboard" element={
+          <ProtectedRoute>
+            <StudentDashboard />
+          </ProtectedRoute>
+        } />
       </Routes>
     </BrowserRouter>
   )
